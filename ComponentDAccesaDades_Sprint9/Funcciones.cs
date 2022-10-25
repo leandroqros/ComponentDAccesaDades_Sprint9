8f6e5be77d03e37d017a6f38f547de83633e8bcc
Leandro Queiroz <leandroqros@gmail.com>
2022-10-25 17:55:17 +0200

    Agregar archivos de proyecto.

---
+++ ComponentDAccesaDades_Sprint9/Funcciones.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ComponentDAccesaDades_Sprint9
+{
+    class Funcciones
+    {
+        //Variables universales
+        SqlConnection conect;
+        SqlDataAdapter tablaTemporal;
+        DataSet dts = new DataSet();
+
+        //Funcion de Conectar
+        public void Connectar()
+        {
+            string connectionString = "Data Source=CAFUNEPORTATIL\\SQLEXPRESS;Initial Catalog=Species;Integrated Security=True";
+            conect = new SqlConnection(connectionString);
+            conect.Open();
+        }
+        
+        public DataSet PortarTaula(string nombreTabla)
+        {
+            string query;
+            query = "select * from *";
+            tablaTemporal = new SqlDataAdapter(query, conect);
+
+            tablaTemporal.Fill(dts, nombreTabla);
+            return dts;
+        }
+
+        public DataSet portarPerConsulta(string query)
+        {
+            string consulta;
+            consulta = query;
+            tablaTemporal = new SqlDataAdapter(query, conect);
+
+            tablaTemporal.Fill(dts);
+            return dts;
+        }
+
+        public DataSet portarPerConsulta(string query, string nomTabla)
+        {
+            string consulta;
+            consulta = query;
+            tablaTemporal = new SqlDataAdapter(query, conect);
+
+            tablaTemporal.Fill(dts, nomTabla);
+            return dts;
+        }
+
+    }
+}
